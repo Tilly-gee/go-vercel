ff44a8e19b9f1bf73eaeb7b53b648c0a0d086acf
Tilly-gee <104358587+Tilly-gee@users.noreply.github.com>
2022-10-07 11:53:22 +0100

    update docs import

--- api/vercel.go
+++ api/vercel.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	_ "golang-vercel/docs"
+	docs "github.com/golang-vercel/docs"
 
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
